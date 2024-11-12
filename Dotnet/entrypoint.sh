bdd9fad0d5ad95b0197ce185518a6bd97d49a3bf
Andre <andretoddriddle@hotmail.com>
2024-11-12 00:56:49 +0000

    Added Dotnet entrypoint

---
+++ Dotnet/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# set this variable, dotnet needs it even without it it reports to `dotnet --info` it can not start any aplication without this
+export DOTNET_ROOT=/usr/share/
+
+# Print current dotnet version
+dotnet --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
